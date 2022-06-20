ce617960486747f2d963c0e5346ba5b4474c5138
geertco <geert.cornu@gmail.com>
2022-06-20 15:06:04 +0200

    9 files modified navigation completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,15 +9,6 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  /*
-  hero = 'Windstorm';
-  */
-  /*hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };*/
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
   constructor(
@@ -27,11 +18,6 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
